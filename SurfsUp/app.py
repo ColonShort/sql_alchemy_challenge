cb2e2b75c69c3dc5e7a01f262b6eed17da79f880
ColonShort <113563696+ColonShort@users.noreply.github.com>
2023-04-04 21:18:41 -0400

    4/4/23

---
+++ SurfsUp/app.py
@@ -0,0 +1,42 @@
+# Import the dependencies.
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# home route
+@app.route("/")
+def home():
+    return("Welcome to the Hawaii Climate Analusis Local API")
+
+## app launcher
+if __name__ == '__main__':
+        app.run()
+
+
+
+#################################################
+# Database Setup
+#################################################
+
+
+# reflect an existing database into a new model
+
+# reflect the tables
+
+
+# Save references to each table
+
+
+# Create our session (link) from Python to the DB
+
+
+#################################################
+# Flask Setup
+#################################################
+
+
+
+
+#################################################
+# Flask Routes
+#################################################
